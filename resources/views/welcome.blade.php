94a3033e92ea1d22c4217a732efbd8616a8bc1b9
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2019-11-17 14:09:39 +0800

    welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zuhaily Ramzi deployed!
                 </div>
 
                 <div class="links">
